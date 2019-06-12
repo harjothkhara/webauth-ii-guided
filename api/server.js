728c2def86dec23dbf4526ce93f5f40b5f0a13c2
harjothkhara <harjoth.khara@gmail.com>
2019-06-12 10:12:02 -0700

    added KnexSessionStore to connect session to database instead of memory, so everytime server restarts session data isn't lost.

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const session = require('express-session'); //<<<<<<<<<<<<<<<<
+const KnexSessionStore = require('connect-session-knex')(session);// <<<<<<<<<<installed library and pass session instance to it (currying)-- allow us to connect sessions to db instead of in mememory
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -18,6 +19,12 @@ const sessionConfig = {
   },
   resave: false, // resave session even if it didn't change
   saveUninitialized: true, // create new sessions automatically, make sure to comply with law
+ 
+  store: new KnexSessionStore({ //<<<<<<<<<<<<<<<<<<<<< connecting session to database instead of on memory
+    knex: require('../database/dbConfig.js'),
+    createtable: true, //if its not there create it for me
+    clearInterval: 1000 * 60 * 15, //clear out expired session in milliseconds
+  })
 };
 
 //middleware
@@ -35,3 +42,7 @@ server.get('/', (req, res) => {
 });
 
 module.exports = server;
+
+//common ways to store session data in the server
+//memory, memory cache server, database server
+//client <--> server <----> session store
